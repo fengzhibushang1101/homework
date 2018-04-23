c688f260b31edfaac07c64859cdf4557e0884a5b
yitian <fengzhibushang@163.com>
2018-04-23 08:54:58 +0800

    complete double_cola

--- Double_Cola.py
+++ Double_Cola.py
@@ -6,3 +6,25 @@
  @Software: PyCharm
  @Description: 
 """
+
+def whoIsNext(names, r):
+    length = len(names)
+    count = 1
+    while r / length >= 1:
+        r -= length
+        length = length * 2
+        count = count * 2
+    return names[int((r-1)/count)]
+names = ['Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard']
+r = 1802
+print whoIsNext(names, r)
+
+
+
+def whoIsNext2(names, r):
+    length = len(names)
+    while r > length:
+        r = (r-length+1) / 2
+    return names[r-1]
+
+    # your code
